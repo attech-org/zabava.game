1d96dcc9ace103d0dbfdeb99a0606be0620c2bc4
Alexandr Tovmach <alexandrtovmach@gmail.com>
2022-12-01 22:52:32 +0100

    feat: Prettified wired types

--- src/wired.d.ts
+++ src/wired.d.ts
@@ -1,4 +1,44 @@
-interface WiredButton {
+interface WiredButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   elevation: number;
+  disabled?: boolean;
+}
+interface WiredCalendar extends React.HTMLAttributes<HTMLElement> {
+  elevation: number;
+  selected?: string;
+  firstdate?: string;
+  lastdate?: string;
+  locale?: string;
   disabled: boolean;
+  initials: boolean;
+  value?: { date: Date; text: string };
+  format: Function;
+}
+interface WiredCard extends React.HTMLAttributes<HTMLElement> {
+  elevation: number;
+  fill?: string;
+}
+interface WiredCheckbox extends React.HTMLAttributes<HTMLElement> {
+  checked: boolean;
+  disabled: boolean;
+}
+interface WiredCombo extends React.HTMLAttributes<HTMLElement> {
+  disabled: boolean;
+  selected?: string;
+}
+interface WiredDialog extends React.HTMLAttributes<HTMLElement> {
+  elevation: number;
+  open: boolean;
+}
+interface WiredDivider extends React.HTMLAttributes<HTMLElement> {
+  elevation: number;
+}
+interface WiredFab extends React.HTMLAttributes<HTMLElement> {
+  disabled: boolean;
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any;
+    "wired-button": WiredButton;
+  }
 }
