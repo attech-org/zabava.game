0e1616b83f7f2d78479ec05367ac30abbb584618
Artem Ligerko <artmarket81@gmail.com>
2022-12-01 23:47:50 +0200

    fix: some fixes

--- src/files.d.ts
+++ src/files.d.ts
@@ -1,14 +1,6 @@
 declare namespace JSX {
   interface IntrinsicElements {
     [elemName: string]: any;
-    // "wired-button": WiredButton;
-    "wired-icon-button": WiredIconButton;
-    "wired-image": WiredImage;
-    "wired-input": WiredInput;
-    "wired-link": WiredLink;
-    "wired-listbox": WiredListbox;
-    "wired-item": WiredItem;
-    "wired-progress": WiredProgress;
-    "wired-radio": WiredRadio;
+    "wired-button": WiredButton;
   }
 }
