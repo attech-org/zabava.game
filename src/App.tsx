69647a7b6a7a8039b6a96abacde768c399d30ed9
Alexandr Tovmach <alexandrtovmach@gmail.com>
2022-12-01 01:18:54 +0100

    feat: Added UI components; Completed basic demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+import "wired-elements";
 
-function App() {
+import words from "./content/alias_words.json";
+
+const Main = styled.main`
+  margin: 0 auto;
+  max-width: 1200px;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CardBoard = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+
+  wired-card {
+    width: 300px;
+    height: 400px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 3rem;
+  }
+`;
+
+const CardBoardActions = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0.5rem 0;
+`
+
+const App: React.FunctionComponent = () => {
+  const [isGameRunning, setIsGameRunning] = useState(false);
+  const [wordIdx, setWordIdx] = useState(0);
+
+  const handleStartGameButtonClick = () => {
+    setIsGameRunning(!isGameRunning);
+  };
+
+  const handleNextWordClick = () => {
+    setWordIdx(Math.min(wordIdx + 1, words.length - 1));
+  };
+
+  const handlePrevWordClick = () => {
+    setWordIdx(Math.max(wordIdx - 1, 0));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      {isGameRunning ? (
+        <CardBoard>
+          <wired-card elevation="3">{words[wordIdx]}</wired-card>
+          <CardBoardActions>
+            <wired-button elevation="2" onClick={handlePrevWordClick}>
+              prev
+            </wired-button>
+            <wired-button elevation="2" onClick={handleNextWordClick}>
+              next
+            </wired-button>
+          </CardBoardActions>
+        </CardBoard>
+      ) : (
+        <wired-button elevation="2" onClick={handleStartGameButtonClick}>
+          start game
+        </wired-button>
+      )}
+    </Main>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,7 @@
+const Card: React.FunctionComponent = () => {
+  return (
+    <div>Card</div>
+  )
+}
+
+export default Card;
