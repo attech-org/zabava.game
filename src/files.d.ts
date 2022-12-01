e36603ce5e075b09b358a857aec3e0da296083e0
Artem Ligerko <artmarket81@gmail.com>
2022-12-01 23:43:07 +0200

    feat: typed wired components

--- src/files.d.ts
+++ src/files.d.ts
@@ -1,6 +1,14 @@
 declare namespace JSX {
   interface IntrinsicElements {
     [elemName: string]: any;
-    "wired-button": WiredButton;
+    // "wired-button": WiredButton;
+    "wired-icon-button": WiredIconButton;
+    "wired-image": WiredImage;
+    "wired-input": WiredInput;
+    "wired-link": WiredLink;
+    "wired-listbox": WiredListbox;
+    "wired-item": WiredItem;
+    "wired-progress": WiredProgress;
+    "wired-radio": WiredRadio;
   }
 }
