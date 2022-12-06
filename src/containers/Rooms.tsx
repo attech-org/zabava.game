7e3b3dcac4d84979709144ba03338aaa2f939a4f
KirillSerg <kirill.sergienko1989@gmail.com>
2022-12-07 00:46:54 +0200

    added small part of left side

--- src/containers/Rooms.tsx
+++ src/containers/Rooms.tsx
@@ -1,14 +1,48 @@
 import styled from "styled-components";
 import { CardBoard, Main } from "./Home";
 
+const Checkbox = styled.input`
+  // background-color: green;
+  // color: red;
+  // border-color: blue;
+
+  // &:disabled {
+  //   background-color: green;
+  //   color: red;
+  //   border-color: blue;
+  // };
+`;
+
+const Player = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
 const Rooms: React.FunctionComponent = () => {
+
+  //temporary mock data
+  const players = [
+    {nickname: "Maks", ready: true},
+    {nickname: "Nikola", ready: false},
+    {nickname: "Lora", ready: true},
+  ]
 
   return (
     <Main>
       <CardBoard>
-        <h2>Joined players</h2>
+        <h2>{`Joined players: ${players.length}`}</h2>
         <wired-card>
-
+          {
+            players.map((player, index) => {
+              return (
+                <Player key={index}>
+                  {/* <wired-radio></wired-radio> */}
+                  <Checkbox type="checkbox" checked={player.ready} disabled />
+                  <h3>{player.nickname}</h3>
+                </Player>
+            )})
+          }
         </wired-card>
       </CardBoard>
       <CardBoard>
