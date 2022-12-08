51b3034987ebe279a84e918c4668ad3c0a5cb133
Alexandr Tovmach <alexandrtovmach@gmail.com>
2022-12-08 23:38:30 +0100

    feat: Added teams generate page

--- src/containers/games/Alias/Teams.tsx
+++ src/containers/games/Alias/Teams.tsx
@@ -1,6 +1,8 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+import { WiredSlider } from "wired-elements";
 import styled from "styled-components";
-import { AliasContext } from ".";
+import { chunk } from "lodash";
+import { AliasContext, AliasContextType } from ".";
 
 const Main = styled.main`
   margin: 0 auto;
@@ -9,18 +11,87 @@ const Main = styled.main`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  wired-card {
+    width: 300px;
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const VerticalList = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
 `;
 
 interface AliasTeamsPageProps {}
 
 const AliasTeamsPage: React.FunctionComponent<AliasTeamsPageProps> = () => {
+  const sliderRef = useRef<WiredSlider>();
+  const [teamSize, setTeamSize] = useState<number>(2);
+  const [teams, setTeams] = useState<AliasContextType["teams"]>([]);
   const { players, role } = useContext(AliasContext);
-  const isHost = role === 'admin'; // enable team edit if true, and disable if false
+  const isHost = role === "admin"; // enable team edit if true, and disable if false
+
+  useEffect(() => {
+    const sliderEl = sliderRef.current;
+    sliderEl?.addEventListener("change", handleTeamSizeChange);
+    return () => {
+      sliderEl?.removeEventListener("change", handleTeamSizeChange);
+    };
+  }, []);
+
+  const handleTeamSizeChange = (e: any) => {
+    setTeamSize(e.target.value);
+  };
+
+  const handleGenerateTeamsClick = () => {
+    const playerChunks = chunk(players, teamSize);
+    const result = [];
+
+    for (let i = 0; i < playerChunks.length; i++) {
+      result.push({
+        id: i,
+        name: `Team #${i}`,
+        players: playerChunks[i],
+      });
+    }
+    console.log(result);
+    setTeams(result);
+  };
 
   return (
     <Main>
-      {isHost && <wired-button>Generate team name</wired-button>}
-      {players.map(el => el.username)}
+      {teams.length ? (
+        <VerticalList>
+          {teams.map((team) => (
+            <wired-card key={team.id} fill="#fffbcd">
+              {team.name}
+              <VerticalList>
+                {team.players.map(({ id, username }) => (
+                  <wired-item key={id}>{username}</wired-item>
+                ))}
+              </VerticalList>
+            </wired-card>
+          ))}
+        </VerticalList>
+      ) : (
+        <wired-card>
+          <VerticalList>
+            {players.map(({ id, username }) => (
+              <wired-item key={id}>{username}</wired-item>
+            ))}
+          </VerticalList>
+        </wired-card>
+      )}
+      <wired-card>
+        Team Size: {teamSize} {teamSize === 2 && '(classic)'}
+        <wired-slider ref={sliderRef} min={2} max={6} value={teamSize} />
+        <wired-button onClick={handleGenerateTeamsClick}>
+          Generate teams
+        </wired-button>
+      </wired-card>
     </Main>
   );
 };
--- src/wired.d.ts
+++ src/wired.d.ts
@@ -1,26 +1,27 @@
 declare namespace JSX {
   interface IntrinsicElements {
     "wired-button": import("wired-elements").WiredButton | React.ButtonHTMLAttributes<HTMLButtonElement>;
-    "wired-calendar": import("wired-elements").WiredCalendar | React.HTMLAttributes<HTMLElement>;
-    "wired-card": import("wired-elements").WiredCard | React.HTMLAttributes<HTMLElement>;
-    "wired-checkbox": import("wired-elements").WiredCheckbox | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-combo": import("wired-elements").WiredCombo | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-dialog": import("wired-elements").WiredDialog | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-divider": import("wired-elements").WiredDivider | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-fab": import("wired-elements").WiredFab | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-icon-button": import("wired-elements").WiredIconButton | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-image": import("wired-elements").WiredImage | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-input": import("wired-elements").WiredInput | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-link": import("wired-elements").WiredLink | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-listbox": import("wired-elements").WiredListbox | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-progress": import("wired-elements").WiredProgress | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-radio": import("wired-elements").WiredRadio | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-search-input": import("wired-elements").WiredSearchInput | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-slider": import("wired-elements").WiredSlider | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-spinner": import("wired-elements").WiredSpinner | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-tabs": import("wired-elements").WiredTabs | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-textarea": import("wired-elements").WiredTextarea | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-toggle": import("wired-elements").WiredToggle | React.HtmlHTMLAttributes<HTMLElement>;
-    "wired-video": import("wired-elements").WiredVideo | React.HtmlHTMLAttributes<HTMLElement>;
+    "wired-calendar": import("wired-elements").WiredCalendar | React.Attributes<HTMLElement>;
+    "wired-card": import("wired-elements").WiredCard | React.Attributes<HTMLElement>;
+    "wired-checkbox": import("wired-elements").WiredCheckbox | React.Attributes<HTMLElement>;
+    "wired-combo": import("wired-elements").WiredCombo | React.Attributes<HTMLElement>;
+    "wired-dialog": import("wired-elements").WiredDialog | React.Attributes<HTMLElement>;
+    "wired-divider": import("wired-elements").WiredDivider | React.Attributes<HTMLElement>;
+    "wired-fab": import("wired-elements").WiredFab | React.Attributes<HTMLElement>;
+    "wired-icon-button": import("wired-elements").WiredIconButton | React.Attributes<HTMLElement>;
+    "wired-image": import("wired-elements").WiredImage | React.Attributes<HTMLElement>;
+    "wired-input": import("wired-elements").WiredInput | React.Attributes<HTMLElement>;
+    "wired-item": import("wired-elements").WiredItem | React.Attributes<HTMLElement>;
+    "wired-link": import("wired-elements").WiredLink | React.Attributes<HTMLElement>;
+    "wired-listbox": import("wired-elements").WiredListbox | React.Attributes<HTMLElement>;
+    "wired-progress": import("wired-elements").WiredProgress | React.Attributes<HTMLElement>;
+    "wired-radio": import("wired-elements").WiredRadio | React.Attributes<HTMLElement>;
+    "wired-search-input": import("wired-elements").WiredSearchInput | React.Attributes<HTMLElement>;
+    "wired-slider": import("wired-elements").WiredSlider | React.Attributes<HTMLElement>;
+    "wired-spinner": import("wired-elements").WiredSpinner | React.Attributes<HTMLElement>;
+    "wired-tabs": import("wired-elements").WiredTabs | React.Attributes<HTMLElement>;
+    "wired-textarea": import("wired-elements").WiredTextarea | React.Attributes<HTMLElement>;
+    "wired-toggle": import("wired-elements").WiredToggle | React.Attributes<HTMLElement>;
+    "wired-video": import("wired-elements").WiredVideo | React.Attributes<HTMLElement>;
   }
 }
